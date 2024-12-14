4f9c7066cd924f93419b8272031adce0aa70f712
pudupeti naveen <pudupetinaveen18@gmail.com>
2024-12-14 22:35:44 +0530

    OptionalClass in java-8-Stream

--- src/OptionalClass/OptionalClassDemo.java
+++ src/OptionalClass/OptionalClassDemo.java
@@ -22,7 +22,7 @@ public class OptionalClassDemo {
 	//	Optional<String>  emailoptional = Optional.of(email);
 	//	System.out.println(emailoptional);
 		
-		//3.odNullable()
+	//3.odNullable()
 		
 		//Optional<String> emailOptional = Optional.ofNullable(email);
 		//System.out.println(emailOptional);
@@ -42,8 +42,34 @@ public class OptionalClassDemo {
 		String defaultOptional= stringOptional.orElse("drfault@gamil.com");
 		System.out.println(defaultOptional);
 		
+		//orElseGet() method
 		String defaultOptional2 = stringOptional.orElseGet(() ->"default@gmmail.com");
 		System.out.println(defaultOptional2);
+		
+		//orElseThrow() method
+		String optionalObject=stringOptional.orElseThrow(() -> new IllegalArgumentException("Email is not exist"));
+		System.out.println(optionalObject);
+		
+		
+		//if present
+		Optional<String> gender =Optional.of("Male");
+		Optional<String> emptyOptional =Optional.empty();
+		
+		gender.ifPresent((s) ->System.out.println("Value is present"));
+		emptyOptional.ifPresent((s) ->System.out.println("no value present"));
+		
+		if(stringOptional.isPresent()) {
+			System.out.println(stringOptional.get());
+		}else {
+			System.out.println("no value present");
+		}
+		
+		String result =" abc ";
+		if(result != null && result.contains("abc")) {
+			System.out.println(result);
+		}
+		
+		
 		
 	}
 
